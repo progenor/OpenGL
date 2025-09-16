c9b5b232b480b33949f1c3f00c3007d0b0bf9c0e
progenor <nero6126@gmail.com>
2025-09-16 10:44:19 -0400

    first commit

---
+++ Lab1/build/CMakeFiles/Pontok.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Pontok.dir/link.d"
+  "CMakeFiles/Pontok.dir/Pontok.cc.o"
+  "CMakeFiles/Pontok.dir/Pontok.cc.o.d"
+  "Pontok"
+  "Pontok.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Pontok.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
